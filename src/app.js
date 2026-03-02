c27e3ba60e1e4672e359919129e74beba0053a5d
Benjamin Kingsley (RIT Student) <bdk8938@rit.edu>
2026-03-02 12:46:56 -0500

    In class demo complete

--- src/app.js
+++ src/app.js
@@ -7,3 +7,25 @@ const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const app = express();
+
+// compress and decompress all requests and responses
+app.use(compression());
+// static file hosting
+app.use('/assets', express.static(path.resolve(`${__dirname}/../client/`)));
+// no more favicon issues
+app.use(favicon(`${__dirname}/../client/img/favicon.png`));
+
+// query or body parameters are auto parsed for url encoded
+app.use(express.urlencoded({ extended: true }));
+// query or body parameters are auto parsed for JSON
+app.use(express.json());
+
+// hook up router
+router(app);
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Listening on port ${port}`);
+})
+
--- src/router.js
+++ src/router.js
@@ -1,7 +1,19 @@
-const controllers = require('./controllers');
+const controllers = require('./controllers'); // importing index.js
 
 const router = (app) => {
+    // html pages
+    app.get('/page1', controllers.page1);
+    app.get('/page2', controllers.page2);
+    app.get('/', controllers.index);
 
+    // get name
+    app.get('/getName', controllers.getName);
+
+    // catches the rest
+    app.get('/*wild', controllers.notFound);
+
+    // POST Requests
+    app.post('/setName', controllers.setName);
 };
 
 module.exports = router;
